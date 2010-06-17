7c54a743dcc30af4cfdb94536435b813d89d4b3a
Andy Gross <andy@basho.com>
2010-06-17 01:08:45 -0400

    a couple more compatibility macros

--- c_src/erl_nif_compat.h
+++ c_src/erl_nif_compat.h
@@ -13,6 +13,8 @@ extern "C" {
 #define enif_alloc_resource_compat enif_alloc_resource
 #define enif_release_resource_compat enif_release_resource
 #define enif_alloc_binary_compat enif_alloc_binary
+#define enif_alloc_compat enif_alloc
+#define enif_free_compat enif_free
 #endif /* R13B04 */
 
 #if ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION == 0
@@ -28,6 +30,12 @@ extern "C" {
 
 #define enif_alloc_binary_compat(E, S, B) \
     enif_alloc_binary(S, B)
+
+#define enif_alloc_compat(E, S) \
+    enif_alloc(S)
+
+#define enif_free_compat(E, P) \
+    enif_free(P)
 
 #endif /* R14 */
 
